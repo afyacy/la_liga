716288ac2aae07f7278eb3ede75e5ba08ca2adb6
Afyacy <25583917+afyacy@users.noreply.github.com>
2021-12-31 01:37:33 +0000

    completed listing of teams

--- src/components/teams.js
+++ src/components/teams.js
@@ -1,12 +1,34 @@
 // Display teams
-export default function Teams() {
-  return (
-    <div>
-      <ul>
-        <h1>Select a team to see its roster</h1>
-        
-      </ul>
-    </div>
+import React, { Component } from 'react';
+import axios from 'axios';
 
-  )
+export default class Teams extends Component {
+  state = {
+    teams: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.football-data.org/v2/teams`, {
+      headers: {
+        "X-Auth-Token": '5f3ece105a1142af826ed06aa1872c0c'
+      }
+    })
+    .then(res => {
+        const teams = res.data.teams;
+        this.setState({ teams });
+    });
+  }
+
+  render() {
+    console.log(this.teams)
+    return (
+      <div>
+        { 
+          this.state.teams.map(team => 
+          <li key={team.id}>{team.name}</li>
+        )}
+      </div>
+    )
+  }
 }
+

b48e5f32ed8c14af1a0e78c3285494510bd17c71
Afyacy <25583917+afyacy@users.noreply.github.com>
2022-01-03 15:13:26 +0000

    format jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="backgroundImage">
+      <Header />
+      <div className="w-7/12 bg-white flex m-auto px-8 relative pt-8 rounded middleContent">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default App;

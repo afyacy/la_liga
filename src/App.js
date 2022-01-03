758f2f77695020e1b2bc6997eff5b91044223960
Afyacy <25583917+afyacy@users.noreply.github.com>
2022-01-03 15:39:20 +0000

    tailwindcss on production

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import './App.css';
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='backgroundImage'>
+    <div className="backgroundImage">
       <Header />
-      <div className='w-7/12 bg-white flex m-auto px-8 relative pt-8 rounded middleContent'>
-         <Outlet />
+      <div className="w-7/12 bg-white flex m-auto px-8 relative pt-8 rounded middleContent">
+        <Outlet />
       </div>
     </div>
   );
